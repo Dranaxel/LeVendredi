f6757a861413b6659f64922e3c7f2504dbdd64d5
Alexandre Marcé <alexandre.marce@datafab.io>
2022-06-30 06:58:40 +0200

    gather albums an display all

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import spotipy
 import json, urllib.parse
 from spotipy.oauth2 import SpotifyClientCredentials
 
+GENRES = "rap francais"
+
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-releases = spotify.search(
-        q="",
-        limit=50,
-        type='album',
-        market='FR')
+releases = spotify.new_releases(country="FR", limit=50)
 
 #releases = json.load(releases)
 
-print(type(releases))
-print(releases)
+for albums in releases['albums']['items']:
+    for artist in albums['artists']:
+        artist_info = spotify.artist(artist['id'])
+        genres = artist_info["genres"]
+        
+        #if GENRES in genres:
+        #    print(albums['name'], albums['artists'], sep=' - ')
+        print(albums['name'], albums['artists'], sep=' - ')
