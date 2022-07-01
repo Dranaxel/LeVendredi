451a5746194323c6f9a59d2caeee2a1ec5e9620c
Alexandre Marcé <alexandre.marce@datafab.io>
2022-07-01 20:09:16 +0200

    adding date filter

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+import json, urllib.parse, datetime
 import spotipy
-import json, urllib.parse
 from spotipy.oauth2 import SpotifyClientCredentials
 
-GENRES = ["rap francais", "rap marseille"]
+GENRES = ["hip hop", "rap francais", "rap marseille", "french hip hop", "francoton", "pop urbaine", "rap calme", "rap francais nouvelle vague", "swiss hip hop", "rap inde"]
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-releases = spotify.new_releases(country="FR")
+releases = spotify.new_releases(country="FR", limit=50)
+
+today_date = datetime.date.today()
+today_date = str(today_date)
+
 
 def is_wanted_genre(genres):
     return True if genres in GENRES else False
 
-while releases['albums']['next']:
+while releases is not None:
+    print(releases)
     for albums in releases['albums']['items']:
+        album_date = albums['release_date']
         artists_genres = []
 
         for artist in albums['artists']:
@@ -22,10 +28,11 @@ while releases['albums']['next']:
 
         filtered_genres = list(filter(is_wanted_genre, artists_genres))
 
-        if filtered_genres != []:
-            print(albums['name'], albums['artists'][0]['name'], genres, sep=' - ')
+        if filtered_genres != [] and today_date == album_date: 
+            print(albums['name'], albums['artists'][0]['name'], genres, albums['release_date'], sep=' - ')
 
 
         #    if GENRES in genres:
         #        print(albums['name'], albums['artists'][0]['name'], genres, sep=' - ')
     releases = spotify.next(releases['albums'])
+    print("next")
