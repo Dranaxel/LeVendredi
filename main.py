7d89b6b461173c2c66b2ff44d1e9e487972181b2
Alexandre Marcé <alexandre.marce@datafab.io>
2022-07-18 01:50:51 +0200

    modified architecture

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import json, urllib.parse, datetime, logging
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-GENRES = ["rap francais", "rap marseille", "french hip hop", "pop urbaine", "rap calme", "rap francais nouvelle vague", "swiss hip hop", "rap inde"]
-
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 logging.basicConfig(level=logging.INFO)
+GENRES = ("rap francais", "rap marseille", "french hip hop", "pop urbaine", "rap calme", "rap francais nouvelle vague", "swiss hip hop", "rap inde")
+results = dict()
 
-releases = spotify.new_releases(country="FR", limit=50)
-
-today_date = datetime.date.today()
-today_date = str(today_date)
-logging.info(f"Today's date is {today_date}")
+def get_today_date():
+    today_date = datetime.date.today()
+    today_date = str(today_date)
+    logging.info(f"Today's date is {today_date}")
+    return(today_date)
 
 def is_wanted_genre(genres):
     return True if genres in GENRES else False
@@ -23,15 +22,32 @@ def get_genres(album):
             genres.extend(artist_info["genres"])
     return(genres)
 
-while releases is not None:
-    for album in releases['albums']['items']:
-        album_date = album['release_date']
+if __name__ == '__main__':
+    today_date = get_today_date()
 
-        album_genres = get_genres(album)
-        filtered_genres = list(filter(is_wanted_genre, album_genres))
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    releases = spotify.new_releases(country="FR", limit=50)
 
-        if filtered_genres != [] and "2022-07-01" == album_date: 
-        #if filtered_genres != []: 
-            print(album['album_type'], album['name'], album['artists'][0]['name'], album_genres, album['release_date'], sep=' - ')
+    while releases is not None:
+        for album in releases['albums']['items']:
+            album_date = album['release_date']
 
-    releases = spotify.next(releases['albums'])
+            album_genres = get_genres(album)
+            filtered_albums = list(filter(is_wanted_genre, album_genres))
+
+            #if filtered_genres != [] and "2022-07-15" == album_date: 
+            if filtered_albums != []: 
+                results.update(
+                        {
+                            album['name']: 
+                            {
+                                "artist": album['artists'][0]['name'],
+                                "date": album['release_date'],
+                                "genres": album_genres,
+                                "type": album['album_type']
+                            }
+                        }
+                    )
+                print(album['album_type'], album['name'], album['artists'][0]['name'], album_genres, album['release_date'], sep=' - ')
+        releases = spotify.next(releases['albums'])
+    print(results)
