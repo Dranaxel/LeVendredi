ad24273ef863fe8c0896cdf640aae8b0b5e376c2
Alexandre Marcé <alexandre.marce@datafab.io>
2022-10-21 16:46:35 +0200

    gather albums

--- main.py
+++ main.py
@@ -10,27 +10,43 @@ GENRES = {"rap francais", "rap marseille", "french hip hop", "pop urbaine", "rap
 results = dict()
 COUNTRY = os.getenv("COUNTRY", "FR")
 
-logging.info(f"Logging to Spotify")
-try:
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-except BaseException:
-    logging.error('There have been a problem')
-logging.info(f"Successfully logged in Spotify")
+def spotify_connector():
+    logging.info(f"Logging to Spotify")
+    try:
+        spotify_instance = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    except BaseException:
+        logging.error('There have been a problem')
+    logging.info(f"Successfully logged in Spotify")
+    return spotify_instance
 
+def get_artists(genre, spotify):
+    artists_list = set()
+    payload = f'genre: "{genre}"'
+    spotify_artists = spotify.search(q=payload, type="artist", limit=50)
+    while spotify_artists is not None:
+        artists = spotify_artists['artists']['items']
+        artists = [ x["id"] for x in artists]
+        artists_list.update(artists)
+        spotify_artists = spotify.next(spotify_artists['artists'])
+    return artists_list
+
+def search_for_albums(artist_id, spotify):
+    spotify_albums = spotify.artist_albums(artist_id, album_type="album", limit=50)
+    albums = spotify_albums["items"]
+    print(albums)
 
 ls_artists = set()
+spotify = spotify_connector()
 for _ in GENRES:
-    payload = f'genre:"{_}"'
-    spotify_artists = spotify.search(q=payload, type="artist", limit=50)
-    print(spotify_artists['artists']["total"])
-    while spotify.next(spotify_artists['artists']):
-        artists = spotify_artists['artists']['items']
-        artists = [ x["name"] for x in artists]
-        ls_artists.update(artists)
-        spotify_artists = spotify.next(spotify_artists['artists'])
+    print(_)
+    artists = get_artists(_, spotify)
+    ls_artists.update(artists)
 
-print(ls_artists)
 print(len(ls_artists))
+for i in ls_artists:
+    print(i)
+    search_for_albums(i, spotify)
+
 def get_today_date():
     today_date = datetime.date.today()
     today_date = str(today_date)
